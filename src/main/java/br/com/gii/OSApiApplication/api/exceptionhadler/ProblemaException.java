1552ca6f8670de4ecfe9a1a487f163f169443f2f
SESI3DI <giovanasavoia2@gmail.com>
2026-03-04 08:21:50 -0300

    atividade 5

---
+++ src/main/java/br/com/gii/OSApiApplication/api/exceptionhadler/ProblemaException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.gii.OSApiApplication.api.exceptionhadler;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author sesi3dia
+ */
+public class ProblemaException {
+    private Integer status;
+    private LocalDateTime dataHora;
+    private String titulo;
+      
+}
